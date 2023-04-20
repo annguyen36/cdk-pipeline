c0cb5ecb16c2f63a7fd125da225f1ad9fc7ba4c1
Nguyen <nguanv@a07817a90a8c.ant.amazon.com>
2023-04-20 10:35:33 -0700

    fix stage file

--- lib/lambda-stage.ts
+++ lib/lambda-stage.ts
@@ -2,7 +2,7 @@ import { LambdaStack } from './lambda-stack';
 import { Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
-export class PipelineStage extends Stage {
+export class LambdaStage extends Stage {
     constructor(scope: Construct, id: string, props?: StageProps) {
         super(scope, id, props);
 
